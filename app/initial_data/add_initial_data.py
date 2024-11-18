792a7ab1bceea34929ef9f4ec6827788268aa9bf
plus <theshimmer313@gmail.com>
2024-11-18 23:18:38 +0330

    commit before merge

--- app/initial_data/add_initial_data.py
+++ app/initial_data/add_initial_data.py
@@ -19,7 +19,33 @@ def add_super_admin():
 def get_permissions() -> list[Permission]:
     with open("permissions.csv", "r") as mf:
         csvreader = csv.reader(mf, delimiter=" ")
+
         return [Permission(name=p) for line in csvreader for p in line]
+
+
+def test():
+    with open("permissions.csv", "r") as mf:
+        csvreader = csv.reader(mf, delimiter=" ")
+        # ps = json.dumps([{"id": 1, "name": p} for line in csvreader for p in line])
+
+        ls = []
+        permission_id = 1
+        parent_id = 1
+        for category in csvreader:
+
+            for permission in category:
+                ls.append({"id": permission_id, "name": permission, "parent_id": parent_id})
+                permission_id += 1
+            parent_id += 1
+        for i in ls:
+            print(i)
+
+    with open("permissions.json", "w") as mf:
+        ls = json.dumps(ls)
+        mf.write(ls)
+
+
+test()
 
 
 def add_permissions():
